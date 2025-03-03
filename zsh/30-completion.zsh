d3c3367e32f43ecb6b56e32ad079531061024c05
FrankyRay <frankyraymarcellsinaga@gmail.com>
2025-03-04 00:48:40 +0700

    add syntax; chg completion

--- zsh/30-completion.zsh
+++ zsh/30-completion.zsh
@@ -1,2 +1,8 @@
-# Initiate compinit
-autoload -U compinit; compinit
+# Initiate compinit (unused)
+#autoload -U compinit; compinit
+
+# Source zsh-autocomplete
+source /usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+
+bindkey -M menuselect '^I'                menu-complete
+bindkey -M menuselect "$terminfo[kcbt]"   reverse-menu-complete
