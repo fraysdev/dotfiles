3e88615c35712bc25c59544cd17c4ed97f291db7
FrankyRay <frankyraymarcellsinaga@gmail.com>
2025-03-04 14:40:59 +0700

    add fzf functionality

--- zsh/90-sources.zsh
+++ zsh/90-sources.zsh
@@ -3,3 +3,19 @@ eval "$(starship init zsh)"
 
 ## ===[ FZF ]=== ##
 source <(fzf --zsh)
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude='.git'"
+export FZF_DEFAULT_OPTS="--height=50% --layout=default --border=rounded"
+
+# CTRL_T command & opts
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_T_OPTS="--preview 'bat --color=always -n --line-range :500 {}'"
+
+# ALT_C command & opts
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude='.git'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always --icons {} | head -200'"
+
+# Tmux fzf conf
+#export FZF_TMUX_OPTS=" -p90%,70%"
+
+## ===[ ZOXIDE ]=== ##
+eval "$(zoxide init zsh)"
